d74f0fa147cd7c5c22e791c29016b437608619ce
yhw0104 <yhw0104@naver.com>
2023-06-29 16:48:30 +0900

    1st commit

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package com.example.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+// lombok
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+
+    private String message;
+}
---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (endPoint)
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean ("Hello World!");
+    }
+}
